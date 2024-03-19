a6b62fe650e9331e2038c74351419138b89477b8
aasthak236 <aastha@storyproc.com>
2024-03-19 13:43:36 +0530

    Changed client and presenter text

--- src/component/SceneComponents.jsx
+++ src/component/SceneComponents.jsx
@@ -11,6 +11,7 @@ import {
 	SceneLoaderFlags,
 	FreeCamera,
 	Matrix,
+	//PointLight,
 } from '@babylonjs/core';
 
 import styles from '../utils/styles/SceneComponent.module.css';
@@ -113,6 +114,8 @@ const SceneComponent = ({
 
 		// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
 		const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+
+		//const slight = new PointLight('slight', new Vector3(-0.37, 3.05, -9.13), scene);
 
 		// Default intensity is 1. Let's dim the light a small amount
 		light.intensity = 1;
